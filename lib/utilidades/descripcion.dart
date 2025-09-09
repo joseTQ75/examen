c4e7aa1072eb8a3db8f075fcccb6b311e3951faa
joseTQ75 <022100585f@uandina.edu.pe>
2025-09-09 17:44:41 -0500

    Update descripcion.dart

--- lib/utilidades/descripcion.dart
+++ lib/utilidades/descripcion.dart
@@ -1,5 +1,11 @@
+// Importa solo el objeto 'rootBundle' del paquete 'services' de Flutter.
+// 'rootBundle' permite acceder a archivos que están incluidos en los assets de la app.
 import 'package:flutter/services.dart' show rootBundle;
 
+// Define una función asincrónica que recibe la ruta de un archivo como parámetro.
+// Esta función se usa para leer archivos de texto (.txt) desde la carpeta 'assets'.
 Future<String> leerDescripcion(String ruta) async {
+  // Utiliza 'rootBundle.loadString' para cargar el contenido del archivo como una cadena de texto.
+  // El resultado se devuelve directamente como un 'Future<String>'.
   return await rootBundle.loadString(ruta);
 }
